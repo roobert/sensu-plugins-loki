ce11b65415ebb65420ad476afbdda85ef95b3ca5
Rob Wilson <roobert@gmail.com>
2020-01-13 15:09:49 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Sensu Checks for Grafana Loki
+
+## Checks
+
+* `check-logcli.rb` - Run A LogQL query against the Loki API.
+
---
+++ bin/check-logcli.rb
@@ -0,0 +1,47 @@
+#!/usr/bin/env ruby
+
+require 'sensu-plugin/check/cli'
+require 'json'
+
+class CheckLogCLI < Sensu::Plugin::Check::CLI
+  option :warn,
+    short: '-w WARN',
+    proc: proc {|a| a.to_i },
+    default: 1
+
+  option :crit,
+    short: '-c CRIT',
+    proc: proc {|a| a.to_i },
+    default: 1
+
+  option :address,
+    short: '-a ADDRESS',
+    long: '--addr ADDRESS',
+    required: true
+
+
+  option :query,
+    :short => '-q QUERY',
+    :long => '--query QUERY'
+
+  def run
+    command = "#{File.expand_path(File.dirname(__FILE__))}/logcli --addr=#{config[:address]} query '#{config[:query]}'"
+    output = `#{command} 2> /dev/null`
+
+    if $? != 0
+      critical "error running check: #{output}"
+    end
+
+    metrics = JSON.load(output)
+
+    if metrics.nil? or metrics.length < 1
+      critical "invalid response from query: '#{metrics}'"
+    end
+
+    matches = metrics.inject(0) { |count, metric| count + metric["values"][0][1].to_i }
+
+    critical "#{matches} > #{config[:crit]}" if matches > config[:crit]
+    warning "#{matches} > #{config[:warn]}" if matches > config[:warn]
+    ok "#{matches} < warning (#{config[:warn]}) < critical (#{config[:crit]})"
+  end
+end
