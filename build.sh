440f7e140333909d28bb81cc659a4347a743a76d
Rob Wilson <roobert@gmail.com>
2020-01-14 20:54:45 +0000

    adjust build process

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ set -euo pipefail
 
 VERSION=$1
 
+bundle install --path=lib/ --binstubs=bin/ --standalone
 tar vzcf "sensu-plugins-loki-${VERSION}.tgz" bin lib
--- lib/bundler/setup.rb
+++ lib/bundler/setup.rb
@@ -1,11 +1,10 @@
 require 'rbconfig'
-# ruby 1.8.7 doesn't define RUBY_ENGINE
-ruby_engine = defined?(RUBY_ENGINE) ? RUBY_ENGINE : 'ruby'
+ruby_engine = RUBY_ENGINE
 ruby_version = RbConfig::CONFIG["ruby_version"]
 path = File.expand_path('..', __FILE__)
 $:.unshift "#{path}/"
-$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/extensions/x86_64-linux/2.4.0-static/json-2.2.0"
-$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/json-2.2.0/lib"
+$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/extensions/universal-darwin-17/2.3.0/json-2.3.0"
+$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/json-2.3.0/lib"
 $:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/mixlib-cli-1.7.0/lib"
 $:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/sensu-plugin-4.0.0/lib"
-$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/bundler/gems/sensu-plugins-memory-checks-c5391d4ae186/lib"
+$:.unshift "#{path}/.."
